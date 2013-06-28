52605d25955897938690e75ff9938a3f6c589823
Jon Cotton <jcotton@baycitizen.org>
2013-06-28 12:44:28 -0700

    adding development support code

--- armstrong/apps/embeds/tests/embeds_support/urls.py
+++ armstrong/apps/embeds/tests/embeds_support/urls.py
@@ -11,3 +11,9 @@ admin.autodiscover()
 urlpatterns = patterns('',
     (r'^admin/', include(admin.site.urls)),
 )
+
+
+# for shell & runserver: Django 1.3 and 1.4 don't need this, but 1.5 does
+# it will only work if DEBUG is True
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+urlpatterns += staticfiles_urlpatterns()
