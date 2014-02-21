785fdc3e4c4d77868d556ca7b63cb5af93376b1a
Jon Cotton <jc@finaltransfer.net>
2014-02-20 18:56:11 -0800

    Remove improper namespacing.

--- armstrong/apps/embeds/__init__.py
+++ armstrong/apps/embeds/__init__.py
@@ -1,6 +1,3 @@
-from pkgutil import extend_path
-__path__ = extend_path(__path__, __name__)
-
 # Define a logger for this component
 import logging
 logger = logging.getLogger(__name__)
--- armstrong/apps/embeds/backends/__init__.py
+++ armstrong/apps/embeds/backends/__init__.py
@@ -1,6 +1,3 @@
-import pkg_resources
-pkg_resources.declare_namespace(__name__)
-
 from functools import wraps
 
 
