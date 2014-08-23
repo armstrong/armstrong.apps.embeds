08f3967d3b4f13a93d4ecca74af98b330a0c5739
Jon Cotton <jc@finaltransfer.net>
2014-08-22 20:13:32 -0700

    tests Django 1.7 - remove app loading warning
    "RemovedInDjango19Warning: Model class tests.support.models.TypeModel doesn't declare an explicit app_label and either isn't in an application in INSTALLED_APPS or else was imported before its application was loaded."

--- tests/support/models.py
+++ tests/support/models.py
@@ -7,18 +7,28 @@ from armstrong.apps.embeds.fields import EmbedURLField
 class TypeModel(models.Model):
     slug = models.SlugField()
 
+    class Meta:
+        app_label = 'armstrong.apps.embeds'
+
 
 class Parent(models.Model, TemplatesByEmbedTypeMixin):
     response = None
     type = models.ForeignKey(TypeModel, null=True, blank=True)
 
+    class Meta:
+        app_label = 'armstrong.apps.embeds'
+
 
 class Child(Parent):
-    pass
+    class Meta:
+        app_label = 'armstrong.apps.embeds'
 
 
 class CustomFieldModel(models.Model):
     field = EmbedURLField(response_attr="response")
+
+    class Meta:
+        app_label = 'armstrong.apps.embeds'
 
     def __init__(self, *args, **kwargs):
         super(CustomFieldModel, self).__init__(*args, **kwargs)
