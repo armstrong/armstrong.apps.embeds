9989b0b015517a4bffecdcffb426106aab971e2c
Jon Cotton <jc@finaltransfer.net>
2014-08-23 00:03:39 -0700

    explicitly declare MIDDLEWARE_CLASSES because the defaults change in Django 1.7

--- env_settings.py
+++ env_settings.py
@@ -12,6 +12,13 @@ INSTALLED_APPS.extend([
     'django.contrib.messages',
     'django.contrib.sessions'
 ])
+MIDDLEWARE_CLASSES = (
+    'django.middleware.common.CommonMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware'
+)
 
 STATIC_URL = "/static/"
 ROOT_URLCONF = 'tests.support.urls'
